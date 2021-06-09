97258225f5434521aca0034936125f4acfd138ac
Sezhiyan Navarasu <sezhiyan_navarasu@hcl.com>
2021-06-09 20:01:48 +0530

    added a test

--- src/main/java/com/learning/Utils.java
+++ src/main/java/com/learning/Utils.java
@@ -23,4 +23,16 @@ public final class Utils {
 		}
 		return true;
 	}
+
+	public static boolean compare(String[] arr1, String[] arr2) {
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
--- src/main/java/com/learning/datastructure/CombinationsOfLetters.java
+++ src/main/java/com/learning/datastructure/CombinationsOfLetters.java
@@ -1,6 +1,6 @@
 package com.learning.datastructure;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,25 +9,19 @@ import java.util.List;
  */
 
 public class CombinationsOfLetters {
-    public static void main(String[] args) {
-        final int combo = 2;
-        final int curCombo = 0;
-        List<String> strings = Arrays.asList("a", "b", "c", "d");
-        combinations("", strings, combo, curCombo).forEach(a -> System.out.println(a));
-    }
 
-    public static List<String> combinations(String string, List<String> remaining, int combo, int curCombo) {
-        List<String> combos = new ArrayList<>();
-        if (curCombo == combo && !string.isEmpty()) {
-            combos.add(string);
-            return combos;
-        }
+	public static List<String> combinations(String string, List<String> remaining, int combo, int curCombo) {
+		List<String> combos = new ArrayList<>();
+		if (curCombo == combo && !string.isEmpty()) {
+			combos.add(string);
+			return combos;
+		}
 
-        for (int index = 0; index < remaining.size(); index++) {
-            combos.addAll(combinations(string + " " + remaining.get(index),
-                    remaining.subList(index + 1, remaining.size()), combo, curCombo + 1));
-        }
-        return combos;
-    }
+		for (int index = 0; index < remaining.size(); index++) {
+			combos.addAll(combinations(string + remaining.get(index), remaining.subList(index + 1, remaining.size()),
+					combo, curCombo + 1));
+		}
+		return combos;
+	}
 
 }
